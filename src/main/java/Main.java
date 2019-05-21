e6d42154fd513ddca6c7cbb91a292d4958bdc3a0
relistar <mister.relistar@gmail.com>
2019-05-21 17:32:08 +0300

    srhhfhd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,32 @@
 import com.github.chen0040.fpm.AssocRuleMiner;
+import com.github.chen0040.fpm.apriori.Apriori;
+import com.github.chen0040.fpm.data.ItemSet;
 import com.github.chen0040.fpm.data.ItemSets;
 import com.github.chen0040.fpm.data.MetaData;
-import com.github.chen0040.fpm.fpg.FPGrowth;
-import org.apache.commons.lang3.StringUtils;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
+    private static String INPUT_FILES_PATH_PREFIX = "src/main/";
+    private static String OUTPUT_FILES_PATH_PREFIX = "src/main/output/";
+    private static String INPUT_FILE = INPUT_FILES_PATH_PREFIX + "transactions.txt";
+    private static String OUTPUT_FILE_TYPE_POSTFIX = ".txt";
+    private static String DATE_PATTERN = "HH-mm-ss_dd-MM-yyyy";
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+
+
     public static void main(String[] args) throws IOException {
 
         /*FileWriter fileWriter = new FileWriter("src/main/transactions.txt");
 
         Map<String, TreeSet<String>> map = new HashMap<String, TreeSet<String>>();
 
-        Scanner scanner = new Scanner(fr);
+        Scanner scanner = new Scanner(fileReader);
         int i = 0;
 
         while (scanner.hasNextLine() *//*&& i < 100*//*) {
@@ -49,11 +58,11 @@ public class Main {
             fileWriter.write(join + "\n");
         }
 
-        fr.close();
+        fileReader.close();
         fileWriter.close();*/
 
-        FileReader fr = new FileReader("src/main/transactions.txt");
-        Scanner sc = new Scanner(fr);
+        FileReader fileReader = new FileReader(INPUT_FILE);
+        Scanner sc = new Scanner(fileReader);
 
 
         List<List<String>> database = new ArrayList<>();
@@ -66,18 +75,45 @@ public class Main {
             i++;
         }
 
+        File file = new File(OUTPUT_FILES_PATH_PREFIX + generateFileName());
 
-        AssocRuleMiner method = new FPGrowth();
-        method.setMinSupportLevel(3);
+        // if file does not exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+
+        AssocRuleMiner method = new Apriori();
+        method.setMinSupportLevel(5);
 
         MetaData metaData = new MetaData(database);
 
-// obtain all frequent item sets with support level not below 2
-        /*ItemSets frequent_item_sets = method.minePatterns(database, metaData.getUniqueItems());*/
-        ItemSets max_frequent_item_sets = method.findMaxPatterns(database, metaData.getUniqueItems());
-        max_frequent_item_sets.stream().forEach(itemSet -> System.out.println("item-set: " + itemSet));
+        ItemSets frequentItemSets = method.minePatterns(database, metaData.getUniqueItems());
+        List<ItemSet> sets = frequentItemSets.getSets();
 
+        for (ItemSet set : sets) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("{");
+            List<String> items = set.getItems();
+            for (int index = 0; index < items.size(); index++) {
+                stringBuilder.append(items.get(index));
+                if (index != items.size() - 1) {
+                    stringBuilder.append(", ");
+                }
+            }
+            stringBuilder.append("}");
 
-        fr.close();
+            fileWriter.write(stringBuilder.toString() + "\n");
+        }
+
+        fileReader.close();
+        fileWriter.close();
+
+        System.out.println(generateFileName());
+    }
+
+    private static String generateFileName() {
+        return simpleDateFormat.format(new Date()).toString() + OUTPUT_FILE_TYPE_POSTFIX;
     }
 }
